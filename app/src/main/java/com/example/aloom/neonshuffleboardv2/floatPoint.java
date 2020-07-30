4791949061d31a8814ea321e61c3f4ca81f00d69
Brodski <cbrodski@gmail.com>
2020-07-30 16:51:43 -0600

    up to date

---
+++ app/src/main/java/com/example/aloom/neonshuffleboardv2/floatPoint.java
@@ -0,0 +1,18 @@
+package com.example.aloom.neonshuffleboardv2;
+
+/**
+ * Created by Brodski on 4/29/2017.
+ */
+
+public class floatPoint {
+    public float x;
+    public float y;
+    floatPoint (float x2, float y2){
+        x = x2;
+        y = y2;
+    }
+    floatPoint (int x2, int y2){
+        x = x2;
+        y = y2;
+    }
+}
